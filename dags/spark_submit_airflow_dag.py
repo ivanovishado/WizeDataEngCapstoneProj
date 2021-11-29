8cc8efbb1bf19f701dad858458353a5988039fbd
Iván Galaviz <ivanovishado@gmail.com>
2021-11-29 00:06:13 -0600

    Remove param

--- dags/spark_submit_airflow_dag.py
+++ dags/spark_submit_airflow_dag.py
@@ -108,8 +108,7 @@ with DAG(
         task_id="create_emr_cluster",
         job_flow_overrides=JOB_FLOW_OVERRIDES,
         aws_conn_id="aws_default",
-        emr_conn_id="emr_default",
-        region_name="us-east-2"
+        emr_conn_id="emr_default"
     )
 
     step_adder = EmrAddStepsOperator(
